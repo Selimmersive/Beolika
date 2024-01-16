0f31faafbae8c2c8c1d49217163a19ea09db0a41
Selim <hello@selimmersive.com>
2024-01-16 22:24:32 +0100

    add neon-tech plugin

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -1,4 +1,12 @@
 module.exports = ({ env }) => ({
+  'strapi-neon-tech-db-branches': {
+    enabled: true,
+    config: {
+      neonApiKey: "5bast42te1wx7ffdu34lm7bkoxg0gtso1ars25i2n2t6hjd53c87lqgbmz3rhkie", 
+      neonProjectName: "Themify",
+      neonRole: "beolika", 
+    }
+  },
     upload: {
       config: {
         provider: 'cloudinary',
